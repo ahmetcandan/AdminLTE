9144f8a801ef32c0564a6f6796f6355859c9a20f
ahmetcandan <candanahm@gmail.com>
2021-01-13 20:46:19 +0300

    AdminLTE.Model

---
+++ AdminLTE.Model/User.cs
@@ -0,0 +1,14 @@
+﻿using AdminLTE.Interface;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminLTE.Model
+{
+    public class User : IdentityUser, IEntity
+    {
+    }
+}
